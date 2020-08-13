a6d7169b1331c2306232bf3903b8e88aa9730aa7
Jonathan B. Jørgensen <jonathan@buurgaard.dk>
2020-08-13 10:47:55 +0200

    Add vertex buffer abstraction with buffer layouts

--- Hassel/src/Hassel/Application.cpp
+++ Hassel/src/Hassel/Application.cpp
@@ -7,11 +7,46 @@
 #include <glad/glad.h>
 
 
-namespace Hassel {
+namespace Hassel
+{
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application* Application::s_Instance = nullptr;
+
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Hassel::ShaderDataType::None:		return GL_FLOAT;
+			break;
+		case Hassel::ShaderDataType::Float:		return GL_FLOAT;
+			break;
+		case Hassel::ShaderDataType::Float2:	return GL_FLOAT;
+			break;
+		case Hassel::ShaderDataType::Float3:	return GL_FLOAT;
+			break;
+		case Hassel::ShaderDataType::Float4:	return GL_FLOAT;
+			break;
+		case Hassel::ShaderDataType::Mat3:		return GL_FLOAT;
+			break;
+		case Hassel::ShaderDataType::Mat4:		return GL_FLOAT;
+			break;
+		case Hassel::ShaderDataType::Int:		return GL_INT;
+			break;
+		case Hassel::ShaderDataType::Int2:		return GL_INT;
+			break;
+		case Hassel::ShaderDataType::Int3:		return GL_INT;
+			break;
+		case Hassel::ShaderDataType::Int4:		return GL_INT;
+			break;
+		case Hassel::ShaderDataType::Bool:		return GL_BOOL;
+			break;
+		}
+
+		HSL_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
 
 	Application::Application()
 	{
@@ -27,17 +62,31 @@ namespace Hassel {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float verticies[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float verticies[3 * 7] = {
+			-0.5f,	-0.5f,	0.0f,	1.0f,	0.0f,	1.0f,	1.0f,
+			0.5f,	-0.5f,	0.0f,	0.0f,	0.0f,	1.0f,	1.0f,
+			0.0f,	0.5f,	0.0f,	0.0f,	1.0f,	0.0f,	1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(verticies, sizeof(verticies)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
 
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLBaseType(element.Type), element.Normalized ? GL_TRUE : GL_FALSE, layout.GetStride(), (const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indicies[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indicies, sizeof(indicies) / sizeof(uint32_t)));
@@ -46,13 +95,17 @@ namespace Hassel {
 			#version 460 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
 				gl_Position = vec4(a_Position, 1.0);
+
+				v_Color = a_Color;
 			}
 		)";
 
@@ -62,10 +115,12 @@ namespace Hassel {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
--- Hassel/src/Hassel/Renderer/Buffer.h
+++ Hassel/src/Hassel/Renderer/Buffer.h
@@ -2,6 +2,131 @@
 
 namespace Hassel
 {
+	enum class ShaderDataType
+	{
+		None = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
+	};
+
+	static uint32_t ShaderDataTypeSize(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Hassel::ShaderDataType::None:		return 0;
+			break;
+		case Hassel::ShaderDataType::Float:		return 4;
+			break;
+		case Hassel::ShaderDataType::Float2:	return 4 * 2;
+			break;
+		case Hassel::ShaderDataType::Float3:	return 4 * 3;
+			break;
+		case Hassel::ShaderDataType::Float4:	return 4 * 4;
+			break;
+		case Hassel::ShaderDataType::Mat3:		return 4 * 3 * 3;
+			break;
+		case Hassel::ShaderDataType::Mat4:		return 4 * 4 * 4;
+			break;
+		case Hassel::ShaderDataType::Int:		return 4;
+			break;
+		case Hassel::ShaderDataType::Int2:		return 4 * 2;
+			break;
+		case Hassel::ShaderDataType::Int3:		return 4 * 3;
+			break;
+		case Hassel::ShaderDataType::Int4:		return 4 * 4;
+			break;
+		case Hassel::ShaderDataType::Bool:		return 1;
+			break;
+		}
+
+		HSL_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
+	struct BufferElement
+	{
+		std::string Name;
+		ShaderDataType Type;
+		uint32_t Size;
+		uint32_t Offset;
+		bool Normalized;
+
+		BufferElement() {};
+
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
+		{
+		}
+
+		uint32_t GetComponentCount() const
+		{
+			switch (Type)
+			{
+			case Hassel::ShaderDataType::None:		return 0;
+				break;
+			case Hassel::ShaderDataType::Float:		return 1;
+				break;
+			case Hassel::ShaderDataType::Float2:	return 2;
+				break;
+			case Hassel::ShaderDataType::Float3:	return 3;
+				break;
+			case Hassel::ShaderDataType::Float4:	return 4;
+				break;
+			case Hassel::ShaderDataType::Mat3:		return 9;
+				break;
+			case Hassel::ShaderDataType::Mat4:		return 16;
+				break;
+			case Hassel::ShaderDataType::Int:		return 1;
+				break;
+			case Hassel::ShaderDataType::Int2:		return 2;
+				break;
+			case Hassel::ShaderDataType::Int3:		return 3;
+				break;
+			case Hassel::ShaderDataType::Int4:		return 4;
+				break;
+			case Hassel::ShaderDataType::Bool:		return 1;
+				break;
+			}
+
+			HSL_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			return 0;
+		}
+	};
+
+	class BufferLayout
+	{
+	public:
+		BufferLayout() {}
+
+		BufferLayout(std::initializer_list<BufferElement> elements)
+			: m_Elements(elements)
+		{
+			CalculateOffsetsAndStride();
+		}
+
+		inline uint32_t GetStride() const { return m_Stride; }
+		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
+
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
+	private:
+		void CalculateOffsetsAndStride()
+		{
+			uint32_t offset = 0;
+			m_Stride = 0;
+			for (auto& element : m_Elements)
+			{
+				element.Offset = offset;
+				offset += element.Size;
+				m_Stride += element.Size;
+			}
+		}
+
+	private:
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
+	};
+
 	class VertexBuffer
 	{
 	public:
@@ -10,7 +135,12 @@ namespace Hassel
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
+		void SetLayout(const BufferLayout& layout) { m_Layout = layout; };
+		const BufferLayout& GetLayout() const { return m_Layout; }
+
 		static VertexBuffer* Create(float* verticies, uint32_t size);
+	private:
+		BufferLayout m_Layout;
 	};
 
 	class IndexBuffer
@@ -19,7 +149,7 @@ namespace Hassel
 		virtual ~IndexBuffer() {}
 
 		virtual void Bind() const = 0;
-		virtual void Unbind() const= 0;
+		virtual void Unbind() const = 0;
 
 		virtual uint32_t GetCount() const = 0;
 
